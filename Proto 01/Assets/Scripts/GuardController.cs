c95ce3de9853f7c294a62afb8b325c547c7e3745
Robert Gill <envman@gmail.com>
2018-06-20 20:32:38 +0100

    hacky pathing

--- Proto 01/Assets/Scripts/GuardController.cs
+++ Proto 01/Assets/Scripts/GuardController.cs
@@ -1,27 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardController : MonoBehaviour {
 
 	public Camera cam;
 	public UnityEngine.AI.NavMeshAgent agent;
+	public NavPoint currentPoint;
 
 	// Use this for initialization
 	void Start () {
-
+		agent.SetDestination(currentPoint.transform.position);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-
-			if (Physics.Raycast(ray, out hit))
+		float dist = agent.remainingDistance;
+		// if (dist!=Mathf.infinite && agent.pathStatus==NavMeshPathStatus.completed && agent.remainingDistance==0)
+		if (agent.pathStatus == UnityEngine.AI.NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+		{
+			if (currentPoint.next)
 			{
-				agent.SetDestination(hit.point);
+				currentPoint = currentPoint.next;
+				agent.SetDestination(currentPoint.transform.position);
 			}
 		}
+		// if (Input.GetMouseButtonDown(0)) {
+		// 	Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		// 	RaycastHit hit;
+		//
+		// 	if (Physics.Raycast(ray, out hit))
+		// 	{
+		// 		agent.SetDestination(hit.point);
+		// 	}
+		// }
 	}
 }
---
+++ Proto 01/Assets/Scripts/NavPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPoint : MonoBehaviour {
+
+	public NavPoint next;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
