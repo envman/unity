c76206f1b4a37c79db9b7d4971bfa270325fc133
Robert Gill <envman@gmail.com>
2018-06-21 12:54:09 +0100

    added door and keypad system

---
+++ Proto 01/Assets/Scripts/DoorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	public string staticPassword;
+	public KeyPadContoller keyPad;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		this.keyPad.Show(staticPassword);
+	}
+
+	public void Open() {
+		Destroy(gameObject);
+	}
+}
---
+++ Proto 01/Assets/Scripts/KeyPadContoller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyPadContoller : MonoBehaviour {
+
+	public Text display;
+	public DoorController door;
+
+	string code = "";
+	string pass = "1234";
+
+	void Start() {
+		door.keyPad = this;
+		Hide();
+	}
+
+	public void Show(string password) {
+		pass = password;
+		gameObject.SetActive(true);
+	}
+
+	public void Hide() {
+		gameObject.SetActive(false);
+	}
+
+	public void Pressed(string val) {
+		code = code + val;
+		display.text = code;
+
+		Check();
+	}
+
+	public void Reset() {
+		code = "";
+	}
+
+	void Check() {
+		if (code == pass) {
+			display.text = "Correct!";
+
+			door.Open();
+			Hide();
+		}
+	}
+}
