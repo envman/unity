4520c0baab941ca7d6be108b4619823a50b2d9df
Robert Gill <envman@gmail.com>
2018-06-21 22:09:24 +0100

    protos

---
+++ Proto 01/Assets/Scripts/HackingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HackingController : MonoBehaviour {
+
+	public InputField input;
+
+	void Start () {
+		input.onEndEdit.AddListener(delegate { LockInput(input); });
+	}
+
+	void LockInput(InputField theInput) {
+
+		if (input.text == "run script hack") {
+
+		}
+
+		input.text = "";
+	}
+}
